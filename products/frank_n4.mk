22ebdd1ef158e17726ed3eb740b4adcbd4c4bc17
sykopompos <sykopompos@gmail.com>
2015-10-08 20:11:01 -0400

    damn it zephik, stop randomly changing shit on me

--- products/frank_n4.mk
+++ products/frank_n4.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/lge/mako/full_mako.mk)
 
 # Inherit common product files
-$(call inherit-product, vendor/frank/configs/common.mk)
+$(call inherit-product, vendor/frank/products/common.mk)
 
 # Setup device specific product configuration
 PRODUCT_NAME := frank_n4
